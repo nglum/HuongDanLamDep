6811e0b7c58c0880f028d034677b0f5277fd33a3
1871020352 Nguyễn Đức Lâm <combohs12@gmail.com>
2026-03-08 14:05:12 +0700

    feat(client): add Client HomeController and Index view

---
+++ Areas/Client/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebHuongDanLamDep.Areas.Client.Controllers
+{
+	[Area("Client")]
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
