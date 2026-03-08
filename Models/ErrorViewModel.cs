dd8b5162b630bdcdae98e5103137e9723ed9c11d
1871020352 Nguyễn Đức Lâm <combohs12@gmail.com>
2026-03-08 13:45:43 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HuongDanLamDep.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
