f22c0e1a606e657940c7ab63cc6b9b5312b4d8a2
1871020352 Nguyễn Đức Lâm <combohs12@gmail.com>
2026-03-08 23:37:13 +0700

    feat(identity): make ApplicationDbContext inherit IdentityDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,13 @@
-﻿using HuongDanLamDep.Models;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using HuongDanLamDep.Models;
+
 namespace HuongDanLamDep.Data
 {
-	public class ApplicationDbContext : DbContext
+	public class ApplicationDbContext : IdentityDbContext
 	{
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-			: base(options)
-		{
-		}
+			: base(options) { }
 
 		public DbSet<Category> Categories { get; set; }
 		public DbSet<Tag> Tags { get; set; }
