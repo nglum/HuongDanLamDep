4ba902d04f2805ace663d22756a720419318e8b7
1871020352 Nguyễn Đức Lâm <combohs12@gmail.com>
2026-03-08 14:10:55 +0700

    chore(routing): configure area routing

--- Program.cs
+++ Program.cs
@@ -40,6 +40,11 @@ namespace HuongDanLamDep
 			app.UseAuthorization();
 
 			app.MapStaticAssets();
+
+			app.MapControllerRoute(
+				name: "areas",
+				pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
 			app.MapControllerRoute(
 				name: "default",
 				pattern: "{controller=Home}/{action=Index}/{id?}")
