3b1324f0d17a3cfa9d424b47ca23830434a94ef8
1871020352 Nguyễn Đức Lâm <combohs12@gmail.com>
2026-03-08 23:17:29 +0700

    feat(models): add Tutorial model

---
+++ Models/Tutorial.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HuongDanLamDep.Models
+{
+	public class Tutorial
+	{
+		public int TutorialId { get; set; }
+
+		[Required(ErrorMessage = "Bạn chưa nhập tiêu đề")]
+		[StringLength(150)]
+		public string Title { get; set; } = "";
+
+		[Required(ErrorMessage = "Bạn chưa nhập nội dung")]
+		public string Content { get; set; } = "";
+
+		// FK -> Category
+		[Display(Name = "Category")]
+		[Required(ErrorMessage = "Bạn chưa chọn Category")]
+		public int CategoryId { get; set; }
+
+		public Category? Category { get; set; }
+	}
+}
