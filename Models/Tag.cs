6552974482e80f1f287265b58d4aa3aaa8cf8a71
1871020352 Nguyễn Đức Lâm <combohs12@gmail.com>
2026-03-08 23:11:12 +0700

    feat(models): add Tag model

---
+++ Models/Tag.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HuongDanLamDep.Models
+{
+	public class Tag
+	{
+		public int TagId { get; set; }
+
+		[Required(ErrorMessage = "Bạn chưa nhập tên Tag")]
+		public string Name { get; set; } = "";
+	}
+}
