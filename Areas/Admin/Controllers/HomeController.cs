3ed8b2de6bd2e3ef1797bd8e2b97c4b14cc056d7
1871020352 Nguyễn Đức Lâm <combohs12@gmail.com>
2026-03-08 14:03:08 +0700

    feat(admin): add Admin HomeController and Index view

---
+++ Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebHuongDanLamDep.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			ViewData["Title"] = "Admin Dashboard";
+			return View();
+		}
+	}
+}
