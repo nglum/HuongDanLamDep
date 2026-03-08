4eb01e9e1ac4faab2254494ade4310fbbd74d0a2
1871020352 Nguyễn Đức Lâm <combohs12@gmail.com>
2026-03-08 14:00:53 +0700

    feat(routing): add root HomeController to redirect to Client area

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,25 +1,11 @@
-using System.Diagnostics;
-using HuongDanLamDep.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace HuongDanLamDep.Controllers
+namespace WebHuongDanLamDep.Controllers
 {
 	public class HomeController : Controller
 	{
+		// Vào "/" sẽ chuyển sang trang Client
 		public IActionResult Index()
-		{
-			return View();
-		}
-
-		public IActionResult Privacy()
-		{
-			return View();
-		}
-
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
+			=> RedirectToAction("Index", "Home", new { area = "Client" });
 	}
 }
