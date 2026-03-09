454c2b46202af1c4881fd2dfc76d441685b07952
1871020352 Nguyễn Đức Lâm <combohs12@gmail.com>
2026-03-09 22:23:12 +0700

    feat(authz): restrict Admin area to Admin role

--- Program.cs
+++ Program.cs
@@ -66,10 +66,7 @@ namespace HuongDanLamDep
 				IdentitySeed.SeedAsync(scope.ServiceProvider).GetAwaiter().GetResult();
 			}
 			app.Run();
-			using (var scope = app.Services.CreateScope())
-			{
-				IdentitySeed.SeedAsync(scope.ServiceProvider).GetAwaiter().GetResult();
-			}
+
 		}
 	}
 }
