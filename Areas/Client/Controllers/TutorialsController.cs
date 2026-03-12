500abc942679a2a700c9e643b34c328bad3e7fc1
1871020352 Nguyễn Đức Lâm <combohs12@gmail.com>
2026-03-12 15:21:49 +0700

    feat(client): add TutorialsController for tutorial list and detail pages

---
+++ Areas/Client/Controllers/TutorialsController.cs
@@ -0,0 +1,66 @@
+﻿using HuongDanLamDep.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HuongDanLamDep.Areas.Client.Controllers
+{
+	[Area("Client")]
+	public class TutorialsController : Controller
+	{
+		private readonly ApplicationDbContext _context;
+
+		public TutorialsController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		// Danh sách bài viết
+		public async Task<IActionResult> Index(int? categoryId, string? search)
+		{
+			var query = _context.Tutorials
+				.AsNoTracking()
+				.Include(t => t.Category)
+				.AsQueryable();
+
+			if (categoryId.HasValue)
+			{
+				query = query.Where(t => t.CategoryId == categoryId.Value);
+			}
+
+			if (!string.IsNullOrWhiteSpace(search))
+			{
+				query = query.Where(t => t.Title.Contains(search));
+			}
+
+			var tutorials = await query
+				.OrderByDescending(t => t.TutorialId)
+				.ToListAsync();
+
+			ViewBag.Categories = await _context.Categories
+				.AsNoTracking()
+				.OrderBy(c => c.Name)
+				.ToListAsync();
+
+			ViewBag.CurrentCategoryId = categoryId;
+			ViewBag.Search = search;
+
+			return View(tutorials);
+		}
+
+		// Chi tiết bài viết
+		public async Task<IActionResult> Details(int id)
+		{
+			var tutorial = await _context.Tutorials
+				.AsNoTracking()
+				.Include(t => t.Category)
+				.FirstOrDefaultAsync(t => t.TutorialId == id);
+
+			if (tutorial == null)
+			{
+				return NotFound();
+			}
+
+			return View(tutorial);
+		}
+	}
+}
