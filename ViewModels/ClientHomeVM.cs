45add2462d8bd0091f37b42e66dfba4f3d129903
1871020352 Nguyễn Đức Lâm <combohs12@gmail.com>
2026-03-12 14:16:23 +0700

    feat(client): add ClientHomeVM for home page content

---
+++ ViewModels/ClientHomeVM.cs
@@ -0,0 +1,10 @@
+﻿using HuongDanLamDep.Models;
+
+namespace HuongDanLamDep.ViewModels
+{
+	public class ClientHomeVM
+	{
+		public List<Category> Categories { get; set; } = new();
+		public List<Tutorial> LatestTutorials { get; set; } = new();
+	}
+}
